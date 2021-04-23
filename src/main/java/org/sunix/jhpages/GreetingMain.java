528374943e643c815ac92d32abe02bb34b0d01eb
Sun Tan <sutan@redhat.com>
2021-04-23 14:44:05 +0000

    playing with jgit
    Signed-off-by: Sun Tan <sutan@redhat.com>

--- src/main/java/org/sunix/jhpages/GreetingMain.java
+++ src/main/java/org/sunix/jhpages/GreetingMain.java
@@ -1,6 +1,14 @@
 package org.sunix.jhpages;
 
+import java.io.File;
+import java.util.Arrays;
+
 import javax.inject.Inject;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
 
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
@@ -12,16 +20,30 @@ public class GreetingMain implements QuarkusApplication {
     GreetingService service;
 
     @Override
-    public int run(String... args) {
+    public int run(String... args) throws InvalidRemoteException, TransportException, GitAPIException {
         // push the current folder to github
-        if(args.length>0) {
-            System.out.println(service.greeting(String.join(" ", args)));
-        } else {
-            System.out.println(service.greeting("commando"));
-        }
+        // jh-page ./folder/ username repo
+        Git.cloneRepository() //
+                .setURI("https://github.com/jh-pages/jh-pages") //
+                .setBranchesToClone(Arrays.asList("refs/heads/gh-pages"))
+                .setBranch("refs/heads/gh-pages")
+                .setDirectory(new File("/projects/jh-pages/target/test")) //
+                .call();
+        // strategies:
+        // - use jgit
+        // - call git installed in my system
+
+        // Flow:
+        // - check if repo exist, if not create it -> need github lib
+        // - check if gh-pages activated in repo and if not ... create it.
+        // - generate the git url to clone
+        // - check if gh-pages branch exist
+        // - clone the project with only gh-pages (sparse checkout?)
+        // - remove content
+        // - copy content
+        // - add, commit push
 
         return 0;
     }
-
 
 }
