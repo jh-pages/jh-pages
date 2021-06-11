2ca3bcd831e77a15430c91e174e6995da5da13a4
Sun Tan <sutan@redhat.com>
2021-06-11 17:31:22 +0000

    creating an orphan branch
    Signed-off-by: Sun Tan <sutan@redhat.com>

--- src/main/java/org/sunix/jhpages/GitHubService.java
+++ src/main/java/org/sunix/jhpages/GitHubService.java
@@ -5,9 +5,7 @@ import java.io.File;
 import javax.enterprise.context.ApplicationScoped;
 
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.InvalidRemoteException;
-import org.eclipse.jgit.api.errors.TransportException;
+import org.kohsuke.github.GHCreateRepositoryBuilder;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
 import org.kohsuke.github.extras.okhttp3.OkHttpConnector;
@@ -67,7 +65,11 @@ public class GitHubService {
             // gitHub.getUserPublicOrganizations("sunix").get("sunix");
             // orgmy.createRepository(this.repoName).create();
 
-            gitHub.createRepository(this.repoName).create();
+            // gitHub.createRepository(this.repoName).create();
+            new GHCreateRepositoryBuilder(this.repoName, gitHub, "/user/repos") //
+                    .autoInit(true) //
+                    .defaultBranch("gh-pages") //
+                    .create();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -77,24 +79,34 @@ public class GitHubService {
         return this.repoOrgName + "/" + this.repoName;
     }
 
-    
     public void createBranch(String branchName) {
 
-
-
         try {
+
+            deleteDirectory(cloneDirectory);
             Git git = Git.cloneRepository() //
-                    .setURI("https://github.com/"+ getFullRepoName()) //
+                    .setURI("https://github.com/" //
+                    + getFullRepoName()) //
                     .setDirectory(cloneDirectory) //
                     .call();
 
- 
-           //  git.branchCreate().setName(branchName).call();
-           //  git.checkout()
+            git.checkout().setOrphan(true).setName(branchName).call();
+            // git.checkout();
+            // deleteDirectory(cloneDirectory);
 
         } catch (Exception e) {
             throw new RuntimeException("An error occured while trying cloning the repo " + branchName, e);
         }
-	}
+    }
+
+    boolean deleteDirectory(File directoryToBeDeleted) {
+        File[] allContents = directoryToBeDeleted.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        return directoryToBeDeleted.delete();
+    }
 
 }
