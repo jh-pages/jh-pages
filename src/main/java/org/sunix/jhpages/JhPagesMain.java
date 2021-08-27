05b9e5e14f19d37fa68d4581c4b79092e0fbce80
Sun Tan <sutan@redhat.com>
2021-08-27 16:59:42 +0000

    spinner wip

--- src/main/java/org/sunix/jhpages/JhPagesMain.java
+++ src/main/java/org/sunix/jhpages/JhPagesMain.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Ref;
+import org.fusesource.jansi.Ansi;
 import org.jline.terminal.Size;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
@@ -25,13 +26,33 @@ public class JhPagesMain implements Runnable {
     @Override
     public void run() {
 
+    // ⣽ checking if repo `blabla` exists
+    // ✔️ repo `blabla` exists
+    // if it doesn't exists
+    // ✔️ repo `blabla` doesn't exist
+    // ⣽ creating repo `blabla`
+    // ✔️ repo `blabla` created
+    // startTask(new CheckIfRepoExistsTask(repourl))
+
+
+  // - check if gh-pages activated in repo and if not ... create it. -> not needed
+ // - ✔️ generate the git url to clone
+ // - ✔️ check if gh-pages branch exist
+ // - ✔️ clone the project with only gh-pages
+ //   - clone the project
+ //   - if gh-pages branch doesn't exist -> give a try with git CLI to see what we could do when the repo empty. 
+ //- ✔️ remove content
+ //- ✔️ copy content
+ //- ✔️ add, commit push
+        
+
         Display display = buildDisplay();
 
-        char[] spinnerChars = new char[] { '-', '/', '|',  '\\' };
+        char[] spinnerChars = new char[] { '⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷' };
         int i = 0;
-        while (true) {
-            i ++;
-            List<String> lines = Arrays.asList(spinnerChars[i % spinnerChars.length] + " something ....");
+        while (i < 50) {
+            i++;
+            List<String> lines = Arrays.asList(Ansi.ansi().fgBlue().a(spinnerChars[i % spinnerChars.length]).reset() + " something ....");
             display.updateAnsi(lines, 0);
             try {
                 Thread.sleep(200);
@@ -39,6 +60,21 @@ public class JhPagesMain implements Runnable {
                 e.printStackTrace();
             }
         }
+        System.out.println(Ansi.ansi().fgGreen().a("✔️").reset() + " something 1 done !");
+
+        i = 0;
+        while (i < 50) {
+            i++;
+            List<String> lines = Arrays.asList(Ansi.ansi().fgBlue().a(spinnerChars[i % spinnerChars.length]).reset() + " something 2 ....");
+            display.updateAnsi(lines, 0);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(Ansi.ansi().fgGreen().a("✔️").reset() + " something 2 done !");
+
     }
 
     protected Display buildDisplay() {
@@ -65,7 +101,8 @@ public class JhPagesMain implements Runnable {
         if (gitHubService.checkRepoExist()) {
             System.out.println("repo " + gitHubService.getFullRepoName() + " does exist");
         } else {
-            System.out.println("repo " + gitHubService.getFullRepoName() + " does NOT exist ... creating the repo !!!!");
+            System.out
+                    .println("repo " + gitHubService.getFullRepoName() + " does NOT exist ... creating the repo !!!!");
             gitHubService.createRepo();
 
         }
@@ -78,8 +115,6 @@ public class JhPagesMain implements Runnable {
             gitHubService.createBranch("gh-pages");
         }
 
-        
-         
         // strategies:
         // - use jgit
 
