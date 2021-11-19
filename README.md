768b693d1d95cc8fb99be34f59a49da5d9e6aa1b
sutan <sutan@redhat.com>
2021-11-19 18:00:37 +0000

    wip: creating test against gh api
    Signed-off-by: sutan <sutan@redhat.com>

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ The flow to implement:
 
  - ✔️ check if repo exist, if not create it -> need github lib https://github.com/quarkiverse/quarkus-github-api
    - ✔️ creating the repo
- - check if gh-pages activated in repo and if not ... create it. -> not needed
+ - check if gh-pages activated in repo and if not ... create it. -> not needed?
  - ✔️ generate the git url to clone
  - ✔️ check if gh-pages branch exist
  - ✔️ clone the project with only gh-pages
@@ -29,5 +29,12 @@ Live stream: https://www.twitch.tv/happylivecoding
 Replay: https://www.youtube.com/playlist?list=PLAtUK_ilLV_0sutSI_rDW8_fmD1k2lux_
 
 TODO next:
-- also see if other projects using quarkus-github-api contains tests
-- if not, start "copying" wiremock tests from https://github.com/hub4j/github-api/blob/main/src/test/java/org/kohsuke/github/AbstractGitHubWireMockTest.java (because the tests artifacts are not published) 
+- testing wiremock
+- record github interactions
+  - [ ] write the unit tests that will interact for real
+  with github
+    - [x] testing create repo if needed
+    - [ ] test gh-pages branch creation
+  - [ ] making sure that it works
+  - [ ] record the interactions
+  - [ ] mock it in our unit tests
--- src/test/java/org/sunix/jhpages/GitHubServiceTest.java
+++ src/test/java/org/sunix/jhpages/GitHubServiceTest.java
@@ -1,10 +1,13 @@
 package org.sunix.jhpages;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import javax.inject.Inject;
 
 import org.junit.jupiter.api.Test;
+import org.sunix.jhpages.steps.Step;
+import org.sunix.jhpages.steps.StepDisplay;
 
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -16,9 +19,23 @@ public class GitHubServiceTest {
 
     @Test
     void should_return_repoName_without_username() {
-        gitHubService.withFullRepoName("sunix/mywebsite");
+        String githubPagesProjectRef = "sunix/mywebsite";
+        gitHubService.withFullRepoName(githubPagesProjectRef);
         assertEquals(gitHubService.getRepoName(), "mywebsite",
                 "the reponame should be the last part of the full repo name");
+    }
+
+    @Test
+    void should_createGHRepo() {
+        String githubPagesProjectRef = "sunix/mywebsite2";
+        gitHubService.withFullRepoName(githubPagesProjectRef) //
+                .init();
+        if (gitHubService.checkRepoExist()) {
+            throw new Error("the repo " + githubPagesProjectRef + " should not exist in the test context");
+        }
+
+        gitHubService.createRepo();
+        assertTrue(gitHubService.checkRepoExist());
 
     }
 
